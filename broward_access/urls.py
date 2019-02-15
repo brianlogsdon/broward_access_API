2bb8e8c92a779907c274929d53e7debff72851f8
Ecca Urueta <edibles@gmail.com>
2019-02-15 01:12:23 +0000

    Removed drf-yasg

--- broward_access/urls.py
+++ broward_access/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url, include
 from rest_framework.documentation import include_docs_urls
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+#from drf_yasg.views import get_schema_view
+#from drf_yasg import openapi
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+"""
 schema_view = get_schema_view(
    openapi.Info(
       title="BrowardAccess Locations API",
@@ -35,11 +36,12 @@ schema_view = get_schema_view(
    ),
    public=True,
 )
+"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    #path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
